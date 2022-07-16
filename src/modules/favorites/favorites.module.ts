381c4b075bd535ab120f173dcbd63c6bd4d89f48
Tatyana404 <kot.kate2020@gmail.com>
2022-07-17 02:07:25 +0300

    feat: implemented the ability to add and remove artists, albums and tracks to / from favorites

--- src/modules/favorites/favorites.module.ts
+++ src/modules/favorites/favorites.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FavoritesService } from './services/favorites.service';
 import { FavoritesController } from './favorites.controller';
+import { InMemoryDB } from './../../db/inMemoryDB';
 
 @Module({
   controllers: [FavoritesController],
 
-  providers: [FavoritesService],
-
-  exports: [FavoritesService],
+  providers: [FavoritesService, InMemoryDB],
 })
 export class FavoritesModule {}
