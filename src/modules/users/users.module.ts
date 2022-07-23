70d5cf63852515aa194719ec7987a1d4634444a5
Tatyana404 <kot.kate2020@gmail.com>
2022-07-23 18:11:08 +0300

    refactor: removed inMemoryDB from user module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { UsersController } from './users.controller';
-import { InMemoryDB } from './../../db/inMemoryDB';
 
 @Module({
   controllers: [UsersController],
 
-  providers: [UsersService, InMemoryDB],
+  providers: [UsersService],
 })
 export class UsersModule {}
