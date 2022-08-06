e1e6a3ec3f6e462ce2c0bf9036eec17af8447141
Tatyana404 <kot.kate2020@gmail.com>
2022-08-07 00:45:18 +0300

    feat: added CustomLogger to AlbumService

--- src/modules/albums/services/albums.service.ts
+++ src/modules/albums/services/albums.service.ts
@@ -3,23 +3,32 @@ import {
   BadRequestException,
   NotFoundException,
   Injectable,
+  HttpStatus,
 } from '@nestjs/common';
 import { validate as uuidValidate } from 'uuid';
 import { Album } from '@prisma/client';
 import { PrismaService } from './../../prisma/services/prisma.service';
+import { CustomLogger } from './../../logger/services/logger.service';
 import { CreateAlbumDto } from './../dto/create-album.dto';
 import { UpdateAlbumDto } from './../dto/update-album.dto';
 
 @Injectable()
 export class AlbumsService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private logger: CustomLogger) {}
 
   async getAlbums(): Promise<Album[]> {
+    this.logger.debug('getAlbums getting started');
+    this.logger.debug('getAlbums completion work');
     return await this.prisma.album.findMany();
   }
 
   async getAlbum(albumId: string): Promise<Album> {
+    this.logger.debug('getAlbum getting started');
+
     if (!uuidValidate(albumId)) {
+      this.logger.error(
+        `${HttpStatus.BAD_REQUEST} Album id ${albumId} invalid`,
+      );
       throw new BadRequestException(`Album id ${albumId} invalid`);
     }
 
@@ -30,19 +39,26 @@ export class AlbumsService {
     });
 
     if (!album) {
+      this.logger.error(`${HttpStatus.NOT_FOUND} Album ${albumId} not found`);
       throw new NotFoundException(`Album ${albumId} not found`);
     }
 
+    this.logger.debug('getAlbum completion work');
     return album;
   }
 
   async createAlbum(createAlbumDto: CreateAlbumDto): Promise<Album> {
+    this.logger.debug('createAlbum getting started');
+
     if (createAlbumDto.artistId) {
       if (
         !(await this.prisma.artist.findUnique({
           where: { id: createAlbumDto.artistId },
         }))
       ) {
+        this.logger.error(
+          `${HttpStatus.UNPROCESSABLE_ENTITY} Artist ${createAlbumDto.artistId} not found`,
+        );
         throw new UnprocessableEntityException(
           `Artist ${createAlbumDto.artistId} not found`,
         );
@@ -50,6 +66,9 @@ export class AlbumsService {
     }
 
     if (!['name', 'year'].every((field: string) => field in createAlbumDto)) {
+      this.logger.error(
+        `${HttpStatus.BAD_REQUEST} Body does not contain required fields`,
+      );
       throw new BadRequestException('Body does not contain required fields');
     }
 
@@ -57,6 +76,7 @@ export class AlbumsService {
       data: { ...createAlbumDto },
     });
 
+    this.logger.debug('createAlbum completion work');
     return newAlbum;
   }
 
@@ -64,7 +84,12 @@ export class AlbumsService {
     albumId: string,
     updateAlbumDto: UpdateAlbumDto,
   ): Promise<Album> {
+    this.logger.debug('updateAlbum getting started');
+
     if (!uuidValidate(albumId)) {
+      this.logger.error(
+        `${HttpStatus.BAD_REQUEST} Album id ${albumId} invalid`,
+      );
       throw new BadRequestException(`Album id ${albumId} invalid`);
     }
 
@@ -75,6 +100,7 @@ export class AlbumsService {
     });
 
     if (!album) {
+      this.logger.error(`${HttpStatus.NOT_FOUND} Album ${albumId} not found`);
       throw new NotFoundException(`Album ${albumId} not found`);
     }
 
@@ -85,15 +111,22 @@ export class AlbumsService {
       data: { ...updateAlbumDto },
     });
 
+    this.logger.debug('updateAlbum completion work');
     return updateAlbum;
   }
 
   async deleteAlbum(albumId: string): Promise<void> {
+    this.logger.debug('deleteAlbum getting started');
+
     if (!uuidValidate(albumId)) {
+      this.logger.error(
+        `${HttpStatus.BAD_REQUEST} Album id ${albumId} invalid`,
+      );
       throw new BadRequestException(`Album id ${albumId} invalid`);
     }
 
     if (!(await this.prisma.album.findUnique({ where: { id: albumId } }))) {
+      this.logger.error(`${HttpStatus.NOT_FOUND} Track ${albumId} not found`);
       throw new NotFoundException(`Track ${albumId} not found`);
     }
 
@@ -102,5 +135,7 @@ export class AlbumsService {
         id: albumId,
       },
     });
+
+    this.logger.debug('deleteAlbum completion work');
   }
 }
