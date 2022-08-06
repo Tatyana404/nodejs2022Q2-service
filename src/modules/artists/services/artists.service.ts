aebf304c97fb62f12194a12aad13596d1f2a4c3a
Tatyana404 <kot.kate2020@gmail.com>
2022-08-07 00:20:49 +0300

    feat: added CustomLogger to ArtistService

--- src/modules/artists/services/artists.service.ts
+++ src/modules/artists/services/artists.service.ts
@@ -2,23 +2,32 @@ import {
   BadRequestException,
   NotFoundException,
   Injectable,
+  HttpStatus,
 } from '@nestjs/common';
 import { validate as uuidValidate } from 'uuid';
 import { Artist } from '@prisma/client';
 import { PrismaService } from './../../prisma/services/prisma.service';
+import { CustomLogger } from './../../logger/services/logger.service';
 import { UpdateArtistDto } from './../dto/update-artist.dto';
 import { CreateArtistDto } from './../dto/create-artist.dto';
 
 @Injectable()
 export class ArtistsService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private logger: CustomLogger) {}
 
   async getArtists(): Promise<Artist[]> {
+    this.logger.debug('getArtists getting started');
+    this.logger.debug('getArtists completion work');
     return await this.prisma.artist.findMany();
   }
 
   async getArtist(artistId: string): Promise<Artist> {
+    this.logger.debug('getArtist getting started');
+
     if (!uuidValidate(artistId)) {
+      this.logger.error(
+        `${HttpStatus.BAD_REQUEST} Artist id ${artistId} invalid`,
+      );
       throw new BadRequestException(`Artist id ${artistId} invalid`);
     }
 
@@ -29,16 +38,23 @@ export class ArtistsService {
     });
 
     if (!artist) {
+      this.logger.error(`${HttpStatus.NOT_FOUND} Artist ${artistId} not found`);
       throw new NotFoundException(`Artist ${artistId} not found`);
     }
 
+    this.logger.debug('getArtist completion work');
     return artist;
   }
 
   async createArtist(createArtistDto: CreateArtistDto): Promise<Artist> {
+    this.logger.debug('createArtist getting started');
+
     if (
       !['name', 'grammy'].every((field: string) => field in createArtistDto)
     ) {
+      this.logger.error(
+        `${HttpStatus.BAD_REQUEST} Body does not contain required fields`,
+      );
       throw new BadRequestException('Body does not contain required fields');
     }
 
@@ -46,6 +62,7 @@ export class ArtistsService {
       data: { ...createArtistDto },
     });
 
+    this.logger.debug('createArtist completion work');
     return newArtist;
   }
 
@@ -53,7 +70,12 @@ export class ArtistsService {
     artistId: string,
     updateArtistDto: UpdateArtistDto,
   ): Promise<Artist> {
+    this.logger.debug('updateArtist getting started');
+
     if (!uuidValidate(artistId)) {
+      this.logger.error(
+        `${HttpStatus.BAD_REQUEST} Artist id ${artistId} invalid`,
+      );
       throw new BadRequestException(`Artist id ${artistId} invalid`);
     }
 
@@ -64,6 +86,7 @@ export class ArtistsService {
     });
 
     if (!artist) {
+      this.logger.error(`${HttpStatus.NOT_FOUND} Artist ${artistId} not found`);
       throw new NotFoundException(`Artist ${artistId} not found`);
     }
 
@@ -74,16 +97,23 @@ export class ArtistsService {
       data: { ...updateArtistDto },
     });
 
+    this.logger.debug('updateArtist completion work');
     return updateArtist;
   }
 
   async deleteArtist(artistId: string): Promise<void> {
+    this.logger.debug('deleteArtist getting started');
+
     if (!uuidValidate(artistId)) {
+      this.logger.error(
+        `${HttpStatus.BAD_REQUEST} Artist id ${artistId} invalid`,
+      );
       throw new BadRequestException(`Artist id ${artistId} invalid`);
     }
 
     if (!(await this.prisma.artist.findUnique({ where: { id: artistId } }))) {
-      throw new NotFoundException(`Track ${artistId} not found`);
+      this.logger.error(`${HttpStatus.NOT_FOUND} Artist ${artistId} not found`);
+      throw new NotFoundException(`Artist ${artistId} not found`);
     }
 
     await this.prisma.artist.delete({
@@ -91,5 +121,7 @@ export class ArtistsService {
         id: artistId,
       },
     });
+
+    this.logger.debug('deleteArtist completion work');
   }
 }
